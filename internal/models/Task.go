6070954afcc522e17f893ba192e7b87c8f68c8c5
becxq <noname@gnida.com>
2026-06-17 18:07:40 +0500

    added interface for TaskRepos

--- internal/models/Task.go
+++ internal/models/Task.go
@@ -1,10 +1,17 @@
 package models
 
-type Task struct{
-		Name string `json:name`
-		Priority int `json:priority`
-		Due time.Time `json:time`
-		Status bool `json:status`
+type Task struct {
+	ID       string    `json:"id"`
+	Name     string    `json:"name"`
+	Priority int       `json:"priority"`
+	Due      time.Time `json:"time"`
+	Status   bool      `json:"status"`
 }
 
-
+type TaskRepository interface {
+	Create(task Task) error
+	Remove(id string) error
+	GetAll() ([]Task, error)
+	Check(id string) error
+	Clear() error
+}
