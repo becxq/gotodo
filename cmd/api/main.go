a7a518959018d220760189c1af10e758bd4532a5
becxq <noname@gnida.com>
2026-06-17 17:37:10 +0500

    added all commands in service/commands.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,13 @@ import (
 )
 
 func main(){
+		service.RootCmd.AddCommand(service.AddCmd)
+		service.RootCmd.AddCommand(service.RmCmd)
+		service.RootCmd.AddCommand(service.ListCmd)
+		service.RootCmd.AddCommand(service.DoneCmd)
+		service.RootCmd.AddCommand(service.ClearCmd)
+		
+		
 		if err := service.RootCmd.Execute(); err != nil{
 				fmt.Println(err)
 				os.Exit(1)
--- internal/service/commands.go
+++ internal/service/commands.go
@@ -11,3 +11,35 @@ var RootCmd = &cobra.Command{
 				fmt.Println("Root Command")
 		},
 }
+
+var AddCmd = &cobra.Command{
+		Use: "add",
+		Run: func(cmd *cobra.Command, args []string){
+				fmt.Println("Add Command")
+		},
+}
+var RmCmd = &cobra.Command{
+		Use: "rm",
+		Run: func(cmd *cobra.Command, args []string){
+				fmt.Println("Rm Command")
+		},
+}
+var ListCmd = &cobra.Command{
+		Use: "list",
+		Run: func(cmd *cobra.Command, args []string){
+				fmt.Println("List Command")
+		},
+}
+var DoneCmd = &cobra.Command{
+		Use: "done",
+		Run: func(cmd *cobra.Command, args []string){
+				fmt.Println("Done Command")
+		},
+}
+var ClearCmd = &cobra.Command{
+		Use: "clear",
+		Run: func(cmd *cobra.Command, args []string){
+				fmt.Println("Clear Command")
+		},
+}
+
