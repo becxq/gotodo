4c095ebe26522dc5a34f8a7a1feb7a46d45bbf7f
becxq <noname@gnida.com>
2026-06-19 02:07:53 +0500

    os.exit to os.Exit

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-		"fmt"
-		"gotodo/internal/service"
-		"os"
+	"fmt"
+	"gotodo/internal/service"
+	"os"
 )
 
-func main(){
-		service.RootCmd.AddCommand(service.AddCmd)
-		service.RootCmd.AddCommand(service.RmCmd)
-		service.RootCmd.AddCommand(service.ListCmd)
-		service.RootCmd.AddCommand(service.DoneCmd)
-		service.RootCmd.AddCommand(service.ClearCmd)
-		
-		
-		if err := service.RootCmd.Execute(); err != nil{
-				fmt.Println(err)
-				os.Exit(1)
-		}
+func main() {
+	service.RootCmd.AddCommand(service.AddCmd)
+	service.RootCmd.AddCommand(service.RmCmd)
+	service.RootCmd.AddCommand(service.ListCmd)
+	service.RootCmd.AddCommand(service.DoneCmd)
+	service.RootCmd.AddCommand(service.ClearCmd)
+
+	if err := service.RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
