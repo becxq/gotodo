ab93d57e6b50615696d8861ba8622b271f02202d
becxq <noname@gnida.com>
2026-06-19 01:53:35 +0500

    distingueshed repository in few files

---
+++ internal/repositories/delete.go
@@ -0,0 +1,37 @@
+package repository
+
+import (
+	"encoding/json"
+	"errors"
+	"gotodo/internal/models"
+	"os"
+)
+
+func (r *Repository) Delete(id string) error {
+	tasks, err := r.GetAll()
+	if err != nil {
+		return err
+	}
+
+	var updatedTasks []models.Task
+	found := false
+
+	for _, task := range tasks {
+		if task.ID == id {
+			found = true
+			continue
+		}
+		updatedTasks = append(updatedTasks, task)
+	}
+
+	if !found {
+		return errors.New("cannot delete: task not found")
+	}
+
+	data, err := json.MarshalIndent(updatedTasks, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(r.filePath, data, 0644)
+}
