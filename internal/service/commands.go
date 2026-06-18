4363041ec66ab4c9cdb5a8538fdab53d88beb747
becxq <noname@gnida.com>
2026-06-19 04:30:19 +0500

    tabs in commands.go

--- internal/service/commands.go
+++ internal/service/commands.go
@@ -1,45 +1,44 @@
 package service
 
 import (
-		"fmt"
-		"github.com/spf13/cobra"
+	"fmt"
+	"github.com/spf13/cobra"
 )
 
 var RootCmd = &cobra.Command{
-		Use: "gotodo",
-		Run: func(cmd *cobra.Command, args []string){
-				fmt.Println("Root Command")
-		},
+	Use: "gotodo",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Root Command")
+	},
 }
 
 var AddCmd = &cobra.Command{
-		Use: "add",
-		Run: func(cmd *cobra.Command, args []string){
-				fmt.Println("Add Command")
-		},
+	Use: "add",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Add Command")
+	},
 }
 var RmCmd = &cobra.Command{
-		Use: "rm",
-		Run: func(cmd *cobra.Command, args []string){
-				fmt.Println("Rm Command")
-		},
+	Use: "rm",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Rm Command")
+	},
 }
 var ListCmd = &cobra.Command{
-		Use: "list",
-		Run: func(cmd *cobra.Command, args []string){
-				fmt.Println("List Command")
-		},
+	Use: "list",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("List Command")
+	},
 }
 var DoneCmd = &cobra.Command{
-		Use: "done",
-		Run: func(cmd *cobra.Command, args []string){
-				fmt.Println("Done Command")
-		},
+	Use: "done",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Done Command")
+	},
 }
 var ClearCmd = &cobra.Command{
-		Use: "clear",
-		Run: func(cmd *cobra.Command, args []string){
-				fmt.Println("Clear Command")
-		},
+	Use: "clear",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Clear Command")
+	},
 }
-
